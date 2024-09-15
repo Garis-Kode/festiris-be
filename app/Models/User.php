93460c6ece361d99d35b4e3508a8540f63476a04
fajar-dev <fajarrivaldi2015@gmail.com>
2024-09-15 15:13:45 +0700

    init: user model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'photo_path',
+        'is_active',
     ];
 
     /**
@@ -42,7 +44,9 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password'          => 'hashed',
+            'photo_path'        => 'array',
+            'is_active'         => 'boolean',
         ];
     }
 }
